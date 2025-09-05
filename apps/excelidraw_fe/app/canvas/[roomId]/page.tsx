45e871d751251f17aa3c88d87a00ff2fda9ca921
Aditya Jha <jhaadi882@gmail.com>
2025-09-05 19:15:31 +0530

    Added rectangle shape

--- apps/excelidraw_fe/app/canvas/[roomId]/page.tsx
+++ apps/excelidraw_fe/app/canvas/[roomId]/page.tsx
@@ -1,8 +1,48 @@
+"use client";
+import { useEffect, useRef } from "react"
 
+export default function Canvas() {
+    const canvasRef = useRef<HTMLCanvasElement>(null)
+    useEffect(() => {
 
+        if (canvasRef.current) {
+            const canvas = canvasRef.current;
+            const context = canvas.getContext("2d");
 
-export default function Canvas(){
+            if (!context) {
+                return;
+            }
+
+            let clicked = false;
+            let startX = 0;
+            let startY = 0;
+
+            canvas.addEventListener("mousedown", (e) => {
+                clicked = true;
+                startX = e.clientX;
+                startY = e.clientY;
+            })
+            canvas.addEventListener("mouseup", (e) => {
+                clicked = false;
+                console.log(e.clientX);
+                console.log(e.clientY);
+            })
+            canvas.addEventListener("mousemove", (e) => {
+                if (clicked) {
+                    let width = e.clientX - startX;
+                    let height = e.clientY - startY;
+
+                    context.clearRect(0,0,canvas.width,canvas.height)
+                    context.strokeRect(startX,startY,width,height)
+
+                }
+
+            })
+
+        }
+    }, [canvasRef])
     return <div>
-        <canvas height={900} width={900}></canvas>
+        <canvas ref={canvasRef} height={900} width={900}
+        ></canvas>
     </div>
 }
