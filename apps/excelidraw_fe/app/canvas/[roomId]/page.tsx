7b13730c64906f9173fb3eb6c6077babc28569b8
Aditya Jha <jhaadi882@gmail.com>
2025-11-06 10:47:14 +0530

    changed canvas route

--- apps/excelidraw_fe/app/canvas/[roomId]/page.tsx
+++ apps/excelidraw_fe/app/canvas/[roomId]/page.tsx
@@ -1,28 +1,14 @@
-
 import { RoomCanvas } from "@/components/RoomCanvas";
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
 
-export default async function CanvasPage({ params }: {
-    params: {
-        roomId: string
-    }
-}) {
-    const roomId = (await params).roomId;
+interface PageProps {
+  params: Promise<{ roomId: string }>;
+}
+
+export default async function CanvasPage({ params }: PageProps) {
+    const {roomId} = await params;
 
 
     return <div>
-        <ToastContainer position="top-center"
-            autoClose={5000}
-            hideProgressBar={false}
-            newestOnTop={false}
-            closeOnClick
-            rtl={false}
-            pauseOnFocusLoss
-            draggable
-            pauseOnHover
-            theme="dark"
-        />
         <RoomCanvas roomId={roomId} />
     </div>
 
